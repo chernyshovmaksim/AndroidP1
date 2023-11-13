0d17732e37be8d79e848b02f62f8e69e92ddb31e
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2023-11-13 15:14:24 +0500

    Changes to be committed: 	new file:   .gitignore 	new file:   .postcssrc 	new file:   .prettierrc 	new file:   package.json 	new file:   src/images/bg-home.png 	new file:   src/images/icons/search.svg 	new file:   src/index.css 	new file:   src/index.html 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js,ts,jsx,tsx,pug}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#019540",
+			},
+		},
+	},
+	plugins: [],
+};
